a8e69db9501e8edbbdaad643b4e24ada4119c3c4
Thomas Massmann <thomas@propertyshelf.com>
2015-03-02 20:25:39 +0100

    Back to development: 0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '0.1'
+version = '0.2.dev0'
 description = 'Plugins for release automation with zest.releaser.'
 long_description = ('\n'.join([
     open('README.rst').read(),
